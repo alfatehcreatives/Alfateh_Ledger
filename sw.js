4eff098e1849ef436b55dcd2fdbefd02037cfc7b
Alfateh Creative Agency <asimlone.bcp@gmail.com>
2026-04-27 00:26:23 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const cacheName = 'alfateh_ledger-v2.01.8';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install & Cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+// Cleanup old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+    ))
+  );
+});
+
+// Stale-While-Revalidate Strategy (Offline Support)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      const fetchPromise = fetch(e.request).then(networkResponse => {
+        return caches.open(cacheName).then(cache => {
+          cache.put(e.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
